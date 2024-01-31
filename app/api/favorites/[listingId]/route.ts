7f2cfb9963bf6f65717c90423b0f158784e00fab
eternale88 <ethanrobbins@ymail.com>
2024-01-31 17:15:17 -0500

    start favorites

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,70 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+// ADD FAVORITE
+export async function POST(request: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds: favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
+
+//delete function
+
+export async function DELETE(equest: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  //removes favorite
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds: favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -8,6 +8,7 @@ import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -82,6 +83,24 @@ const ListingCard: React.FC<ListingCardProps> = ({
           <div className='absolute top-3 right-3'>
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
+        </div>
+        <div className='font-semibold text-lg'>
+          {location?.region}, {location?.label}
+        </div>
+        <div className='font-light text-neutral-500'>
+          {reservationDate || data.category}
+        </div>
+        <div className='flex flex-row items-center gap-1'>
+          <div className='font-semibold'>$ {price}</div>
+          {!reservation && <div className='font-light'>night</div>}
+          {onAction && actionLabel && (
+            <Button
+              small
+              label={actionLabel}
+              disabled={disabled}
+              onClick={handleCancel}
+            />
+          )}
         </div>
       </div>
     </div>
