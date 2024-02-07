ff8dd48d615f317eae27095723ba479e8ee79445
eternale88 <ethanrobbins@ymail.com>
2024-02-07 13:29:34 -0500

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 export { default } from "next-auth/middleware";
 
+//protect routes as user should be logged in to see them
 export const config = {
   matcher: ["/trips", "/reservations", "/properties", "/favorites"],
 };
